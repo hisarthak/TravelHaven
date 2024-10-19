82623b8efab32c971f09ca1eab265bd194e7b95c
Sarthak Pandey <realsarthakpandey1@gmail.com>
2024-10-19 10:45:36 +0530

    chore: clean up code for better readability and maintenance

--- app.js
+++ app.js
@@ -101,7 +101,6 @@ app.get("/" , async (req, res)=>{
     res.render("listings/index.ejs", {allListings, selectedCategory });
      });
 
-
 app.use("/legal" , legalRouter);
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
